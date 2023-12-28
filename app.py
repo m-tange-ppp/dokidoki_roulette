d46d58ded2adf7963afa9ad49825dea7b453a23d
MiTange <tmrevo210@yahoo.co.jp>
2023-12-28 19:01:55 +0900

    add joshi

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import random
 import sqlite3
 
 st.title("ドキドキルーレット")
-st.header("放てことばのきらめき")
 st.text("ことばを3個以上登録して、TokiMekiしてね")
+
+# データベースに接続する
 con = sqlite3.connect("main.db")
 cur = con.cursor()
 cur.execute(
@@ -14,21 +15,38 @@ cur.execute(
     )"""
 )
 
+# ことばを追加する
 with st.form(key="submit_form", clear_on_submit=True):
-    word_input = st.text_input("ことばをスペース区切りで入力してね", placeholder="ことば")
+    word_input_form = st.text_input("ことばをスペース区切りで入力してね", placeholder="ことば")
     submit_button = st.form_submit_button("ことばを登録")
-    if submit_button and word_input != "":
-        word_list = word_input.split()
+    if submit_button and word_input_form != "":
+        word_list = word_input_form.split()
         for i in range(len(word_list)):
             cur.execute("INSERT INTO items (word) VALUES (?)", (word_list[i], ))
             con.commit()
         # print(cur.execute("SELECT * FROM items").fetchall())
 
+# 助詞の設定
+JOSHI_LIST = ["が", "の", "を", "に", "へ", "と", "より", "から", "で", "や"]
+first_joshi_checkbox = st.checkbox("1つ目と2つ目のことばの間に助詞を入れる")
+second_joshi_checkbox = st.checkbox("2つ目と3つ目のことばの間に助詞を入れる")
+
+# 追加した言葉でルーレットを回す
 res = list(x[0] for x in cur.execute("SELECT word FROM items").fetchall())
 if st.button("Let's TokiMeki!!") and len(res) >= 3:
     nums = random.sample(list(res), 3)
-    st.markdown(f"# {nums[0]} {nums[1]} {nums[2]}", )
+    first_joshi = random.choice(JOSHI_LIST)
+    second_joshi = random.choice(JOSHI_LIST)
+    if first_joshi_checkbox and second_joshi_checkbox:
+        st.markdown(f"# {nums[0]} {first_joshi} {nums[1]} {second_joshi} {nums[2]}")
+    elif first_joshi_checkbox and not second_joshi_checkbox:
+        st.markdown(f"# {nums[0]} {first_joshi} {nums[1]} {nums[2]}")
+    elif not first_joshi_checkbox and second_joshi_checkbox:
+        st.markdown(f"# {nums[0]} {nums[1]} {second_joshi} {nums[2]}")
+    else:
+        st.markdown(f"# {nums[0]} {nums[1]} {nums[2]}")
 
+# テーブルをリセットする
 if st.button("ことばのテーブルをリセットする"):
     cur.execute("DELETE FROM items")
     con.commit()
