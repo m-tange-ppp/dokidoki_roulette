efd057f19f53e8a9058c38ddd4c3d6a20088df6b
MiTange <tmrevo210@yahoo.co.jp>
2023-12-29 09:44:47 +0900

    add animation

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 import streamlit as st
 import random
 import sqlite3
+import time
+import threading
 
+# ルーレットのクラス
+class Roulette(threading.Thread):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.should_stop = threading.Event()
+        self.first_joshi = ""
+        self.second_joshi = ""
+        self.words = []
+    
+    def run(self):
+        while not self.should_stop.wait(0):
+            self.first_joshi = random.choice(JOSHI_LIST)
+            self.second_joshi = random.choice(JOSHI_LIST)
+            self.words = random.sample(list(res), 3)
+
+# タイトルと初めの処理
 st.title("ドキドキルーレット")
 st.text("ことばを3個以上登録して、TokiMekiしてね")
+if not "roulette" in st.session_state:
+    st.session_state["roulette"] = None
+roulette = st.session_state["roulette"]
+if not "result" in st.session_state:
+    st.session_state["result"] = ""
+result = st.session_state["result"]
 
-# データベースに接続する
+# データベースに接続
 con = sqlite3.connect("main.db")
 cur = con.cursor()
 cur.execute(
@@ -15,7 +39,7 @@ cur.execute(
     )"""
 )
 
-# ことばを追加する
+# ことばを追加
 with st.form(key="submit_form", clear_on_submit=True):
     word_input_form = st.text_input("ことばをスペース区切りで入力してね", placeholder="ことば")
     submit_button = st.form_submit_button("ことばを登録")
@@ -31,20 +55,33 @@ JOSHI_LIST = ["が", "の", "を", "に", "へ", "と", "より", "から", "で
 first_joshi_checkbox = st.checkbox("1つ目と2つ目のことばの間に助詞を入れる")
 second_joshi_checkbox = st.checkbox("2つ目と3つ目のことばの間に助詞を入れる")
 
-# 追加した言葉でルーレットを回す
+# ルーレットの挙動
 res = list(x[0] for x in cur.execute("SELECT word FROM items").fetchall())
-if st.button("Let's TokiMeki!!") and len(res) >= 3:
-    nums = random.sample(list(res), 3)
-    first_joshi = random.choice(JOSHI_LIST)
-    second_joshi = random.choice(JOSHI_LIST)
-    if first_joshi_checkbox and second_joshi_checkbox:
-        st.markdown(f"# {nums[0]} {first_joshi} {nums[1]} {second_joshi} {nums[2]}")
-    elif first_joshi_checkbox and not second_joshi_checkbox:
-        st.markdown(f"# {nums[0]} {first_joshi} {nums[1]} {nums[2]}")
-    elif not first_joshi_checkbox and second_joshi_checkbox:
-        st.markdown(f"# {nums[0]} {nums[1]} {second_joshi} {nums[2]}")
-    else:
-        st.markdown(f"# {nums[0]} {nums[1]} {nums[2]}")
+if st.button("ルーレットを回す") and len(res) >= 3:
+    roulette = st.session_state["roulette"] = Roulette(daemon=True)
+    roulette.start()
+    st.experimental_rerun()
+if st.button("ルーレットを止める"):
+    roulette.should_stop.set() # set()でTrueに
+    roulette.join() # 前の処理を待つ
+    roulette = st.session_state["roulette"] = None
+    st.experimental_rerun()
+
+if roulette:
+    placeholder = st.empty()
+    while roulette.is_alive():
+        if first_joshi_checkbox and second_joshi_checkbox:
+            gen = f"# {roulette.words[0]} {roulette.first_joshi} {roulette.words[1]} {roulette.second_joshi} {roulette.words[2]}"
+        elif first_joshi_checkbox and not second_joshi_checkbox:
+            gen = f"# {roulette.words[0]} {roulette.first_joshi} {roulette.words[1]} {roulette.words[2]}"
+        elif not first_joshi_checkbox and second_joshi_checkbox:
+            gen = f"# {roulette.words[0]} {roulette.words[1]} {roulette.second_joshi} {roulette.words[2]}"
+        else:
+            gen = f"# {roulette.words[0]} {roulette.words[1]} {roulette.words[2]}"
+        st.session_state["result"] = gen # 生成したものをresultに格納する
+        placeholder.markdown(gen)
+        time.sleep(1)
+st.markdown(result) # resultを表示する
 
 # テーブルをリセットする
 if st.button("ことばのテーブルをリセットする"):
